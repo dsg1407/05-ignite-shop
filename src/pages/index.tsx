aff57cf5f1245524cb3dd4b7ea97129a653234f7
Daniel S Goncalves <dsg1407@gmail.com>
2025-02-04 10:37:45 -0300

    feat: change slider width and add arrow button to drag functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import { HomeContainer, Product } from "@/styles/pages/home";
-import Image from "next/image";
-import {useKeenSlider} from 'keen-slider/react'
+import {
+  ChangeLeftSlideContainer,
+  ChangeRightSlideContainer,
+  HomeContainer,
+  Product,
+  SliderButton,
+} from '@/styles/pages/home'
+import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
 
 import 'keen-slider/keen-slider.min.css'
-import { stripe } from "@/lib/stripe";
-import { GetStaticProps } from "next";
-import Stripe from "stripe";
-import Head from "next/head";
-import { Handbag } from "phosphor-react";
+import { stripe } from '@/lib/stripe'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
+import Head from 'next/head'
+import { CaretLeft, CaretRight, Handbag } from 'phosphor-react'
+import { useState } from 'react'
 
 interface HomeProps {
   products: {
@@ -18,27 +25,41 @@ interface HomeProps {
   }[]
 }
 
-export default function Home({products}: HomeProps) {
-  const [sliderRef] = useKeenSlider({
+export default function Home({ products }: HomeProps) {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    initial: 0,
     slides: {
-      perView: 3,
-      spacing: 48
-    }
+      origin: currentSlide > 0 ? 'center' : 'auto',
+      perView: currentSlide > 0 ? 2 : 1.8,
+      spacing: 48,
+    },
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel)
+    },
   })
 
   return (
     <>
       <Head>
-          <title>Home | Ignite Shop</title>
+        <title>Home | Ignite Shop</title>
       </Head>
 
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        
-
-        {products.map(product => {
+      <HomeContainer
+        ref={sliderRef}
+        className="keen-slider"
+        size={currentSlide > 0 ? 'full' : 'adjusted'}
+      >
+        {products.map((product) => {
           return (
-            <Product className="keen-slider__slide" key={product.id} href={`/product/${product.id}`} prefetch={false}>
-              <Image src={product.imageUrl} alt='' width={520} height={480} />
+            <Product
+              className="keen-slider__slide"
+              key={product.id}
+              href={`/product/${product.id}`}
+              prefetch={false}
+            >
+              <Image src={product.imageUrl} alt="" width={520} height={480} />
 
               <footer>
                 <section>
@@ -47,43 +68,68 @@ export default function Home({products}: HomeProps) {
                 </section>
 
                 <div>
-                  <Handbag size={32} weight="bold"/>
+                  <Handbag size={32} weight="bold" />
                 </div>
-
               </footer>
-          </Product>
-
+            </Product>
           )
         })}
       </HomeContainer>
-    </>
-  );
-}
 
+      {currentSlide > 0 && (
+        <ChangeLeftSlideContainer>
+          <SliderButton
+            type="button"
+            onClick={(e: any) =>
+              e.stopPropagation() || instanceRef.current?.prev()
+            }
+          >
+            <CaretLeft size={48} />
+          </SliderButton>
+        </ChangeLeftSlideContainer>
+      )}
+
+      {currentSlide !== products.length - 1 && (
+        <ChangeRightSlideContainer>
+          <SliderButton
+            type="button"
+            onClick={(e: any) =>
+              e.stopPropagation() || instanceRef.current?.next()
+            }
+          >
+            <CaretRight size={48} />
+          </SliderButton>
+        </ChangeRightSlideContainer>
+      )}
+    </>
+  )
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
-    expand: ['data.default_price']
+    expand: ['data.default_price'],
   })
 
-
-  const products = response.data.map( product => {
+  const products = response.data.map((product) => {
     const price = product.default_price as Stripe.Price
 
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: (price.unit_amount ? price.unit_amount /100 : 0).toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-      })
+      price: (price.unit_amount ? price.unit_amount / 100 : 0).toLocaleString(
+        'pt-BR',
+        {
+          style: 'currency',
+          currency: 'BRL',
+        }
+      ),
     }
   })
   return {
     props: {
-      products
+      products,
     },
-    revalidate: 60 * 60 * 2 // 2 hrs
+    revalidate: 60 * 60 * 2, // 2 hrs
   }
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,14 +1,65 @@
-import { styled } from "..";
-import Link from "next/link";
-
+import { styled } from '..'
+import Link from 'next/link'
 
 export const HomeContainer = styled('main', {
   display: 'flex',
   width: '100%',
-  maxWidth: 'calc(100vw - ((100vw - 1180px) /2))',
-  marginLeft: 'auto',
-  minHeight: 656
+  minHeight: 656,
 
+  variants: {
+    size: {
+      adjusted: {
+        maxWidth: 'calc(100vw - ((100vw - 1180px) /2))',
+        marginLeft: 'auto',
+      },
+      full: {
+        maxWidth: '100%',
+        marginLeft: 0,
+      },
+    },
+  },
+})
+
+export const ChangeRightSlideContainer = styled('div', {
+  background:
+    'linear-gradient(90deg, rgba(18, 18, 20, 0.00) 0%, rgba(18, 18, 20, 0.75) 100%)',
+  position: 'absolute',
+  right: 0,
+  height: '100vh',
+  width: 136,
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: '0 16px',
+})
+
+export const ChangeLeftSlideContainer = styled('div', {
+  background:
+    'linear-gradient(90deg,rgba(18, 18, 20, 0.75) 0%, rgba(18, 18, 20, 0.00) 100%)',
+  position: 'absolute',
+  left: 0,
+  height: '100vh',
+  width: 136,
+  padding: '0 16px',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+})
+
+export const SliderButton = styled('button', {
+  border: 0,
+  background: 'transparent',
+  color: '$gray300',
+
+  cursor: 'pointer',
+  transition: 'all 0.2s',
+
+  '&:hover': {
+    opacity: 0.8,
+    scale: 1.2,
+  },
 })
 
 export const Product = styled(Link, {
@@ -18,14 +69,12 @@ export const Product = styled(Link, {
   position: 'relative',
   overflow: 'hidden',
 
-  
-
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
 
   img: {
-    objectFit: 'cover'
+    objectFit: 'cover',
   },
 
   footer: {
@@ -33,7 +82,7 @@ export const Product = styled(Link, {
     bottom: '0.25rem',
     left: '0.25rem',
     right: '0.25rem',
-    padding: '2rem', 
+    padding: '2rem',
 
     borderRadius: 6,
 
@@ -53,15 +102,15 @@ export const Product = styled(Link, {
       gap: 4,
 
       strong: {
-      fontSize: '$md',
-      color: '$gray100'
-    },
+        fontSize: '$md',
+        color: '$gray100',
+      },
 
-    span: {
-      fontSize: '$xl',
-      fontWeight: 'bold',
-      color: '$green300'
-    }
+      span: {
+        fontSize: '$xl',
+        fontWeight: 'bold',
+        color: '$green300',
+      },
     },
 
     div: {
@@ -70,19 +119,16 @@ export const Product = styled(Link, {
       justifyContent: 'center',
       borderRadius: 6,
 
-
-      padding: '0.75rem', 
+      padding: '0.75rem',
       backgroundColor: '$green500',
-      color: '$white'
-
-      
-    }
+      color: '$white',
+    },
   },
 
   '&:hover': {
     footer: {
       transform: 'translateY(0%)',
       opacity: 1,
-    }
-  }
+    },
+  },
 })
