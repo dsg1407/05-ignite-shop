4e30cbbba4b4dfede2789aafafa38b5df0918f21
Daniel S Goncalves <dsg1407@gmail.com>
2025-02-04 10:41:35 -0300

    fix: adjust title issue

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,12 +1,16 @@
-import { stripe } from "@/lib/stripe"
-import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
-import axios from "axios"
-import { GetStaticPaths, GetStaticProps } from "next"
-import Head from "next/head"
-import Image from "next/image"
-import { useRouter } from "next/router"
-import { useState } from "react"
-import Stripe from "stripe"
+import { stripe } from '@/lib/stripe'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/styles/pages/product'
+import axios from 'axios'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Stripe from 'stripe'
 
 interface ProductProps {
   product: {
@@ -14,21 +18,22 @@ interface ProductProps {
     name: string
     imageUrl: string
     defaultPriceId: string
-    price: string,
+    price: string
     description: string
   }
 }
 
-export default function Product({product}: ProductProps){
-  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
 
-  const {isFallback} = useRouter()
+  const { isFallback } = useRouter()
 
-  async function handleBuyProduct(){
+  async function handleBuyProduct() {
     try {
       setIsCreatingCheckoutSession(true)
       const response = await axios.post('/api/checkout', {
-        priceId: product.defaultPriceId
+        priceId: product.defaultPriceId,
       })
 
       const { checkoutUrl } = response.data
@@ -41,71 +46,73 @@ export default function Product({product}: ProductProps){
   }
 
   if (isFallback) {
-    return (
-      <p>Loading...</p>
-    )
+    return <p>Loading...</p>
   }
 
   return (
     <>
       <Head>
-          <title>{product.name} | Ignite Shop</title>
+        <title>{`${product.name} | Ignite Shop`}</title>
       </Head>
 
       <ProductContainer>
-      <ImageContainer>
-        <Image src={product.imageUrl} alt="" width={520} height={480}/>
-      </ImageContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} alt="" width={520} height={480} />
+        </ImageContainer>
 
-      <ProductDetails>
-        <h1>{product.name}</h1>
-        <span>{product.price}</span>
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
 
-        <p>{product.description}</p>
+          <p>{product.description}</p>
 
-        <button onClick={handleBuyProduct} disabled={isCreatingCheckoutSession}>
-          Comprar agora
-        </button>
-      </ProductDetails>
-    </ProductContainer>
+          <button
+            onClick={handleBuyProduct}
+            disabled={isCreatingCheckoutSession}
+          >
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
     </>
-    
   )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [
-      {params: {id: 'prod_RhZfvvVZ4QuoQN'}}
-    ],
-    fallback: true
+    paths: [{ params: { id: 'prod_RhZfvvVZ4QuoQN' } }],
+    fallback: true,
   }
 }
 
-export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
-  
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
   const productId = params.id
 
   const product = await stripe.products.retrieve(productId, {
-    expand: ['default_price']
+    expand: ['default_price'],
   })
 
   const price = product.default_price as Stripe.Price
 
   return {
-    props: { 
+    props: {
       product: {
         id: product.id,
         name: product.name,
         imageUrl: product.images[0],
         defaultPriceId: price.id,
-        price: (price.unit_amount ? price.unit_amount /100 : 0).toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-        }),
-        description: product.description
-      }
+        price: (price.unit_amount ? price.unit_amount / 100 : 0).toLocaleString(
+          'pt-BR',
+          {
+            style: 'currency',
+            currency: 'BRL',
+          }
+        ),
+        description: product.description,
+      },
     },
-    revalidate: 60 * 60 * 1 // 1 hour
+    revalidate: 60 * 60 * 1, // 1 hour
   }
 }
