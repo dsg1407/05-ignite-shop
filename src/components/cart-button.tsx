65d39233ddb9ac44eb3a81842a7ced6c985708d6
Daniel S Goncalves <dsg1407@gmail.com>
2025-02-04 16:04:59 -0300

    feat: add modal layout

--- src/components/cart-button.tsx
+++ src/components/cart-button.tsx
@@ -1,11 +1,65 @@
-import { CartButtonContainer } from '@/styles/components/cart-button'
-import { Handbag } from 'phosphor-react'
+import {
+  CartButtonContainer,
+  CartList,
+  CartListItem,
+  CartResumeRow,
+  CheckoutButton,
+  CloseModalButton,
+  ModalContainer,
+  ModalOverlay,
+} from '@/styles/components/cart-button'
+import Image from 'next/image'
+import { Handbag, X } from 'phosphor-react'
+import { useState } from 'react'
+
+import imgTest from '@/assets/camisetas/1.png'
 
 export function CartButton() {
+  const [isModalOpen, setIsModalOpen] = useState(true)
+
   return (
-    <CartButtonContainer>
-      <Handbag weight="bold" size={24} />
-      <span>1</span>
-    </CartButtonContainer>
+    <>
+      <CartButtonContainer onClick={() => setIsModalOpen(true)}>
+        <Handbag weight="bold" size={24} />
+        <span>1</span>
+      </CartButtonContainer>
+
+      {isModalOpen && (
+        <>
+          <ModalOverlay onClick={() => setIsModalOpen(false)} />
+          <ModalContainer>
+            <CloseModalButton onClick={() => setIsModalOpen(false)}>
+              <X size={24} />
+            </CloseModalButton>
+
+            <h2>Sacola de compras</h2>
+
+            <CartList>
+              <CartListItem>
+                <Image src={imgTest.src} alt="" width={102} height={93} />
+
+                <div>
+                  <p>Camiseta Beyond the LimitLimit LimitsssLimits</p>
+                  <span>R$ 79,90</span>
+                  <button type="button">Remover</button>
+                </div>
+              </CartListItem>
+            </CartList>
+
+            <CartResumeRow>
+              <p>Quantidade</p>
+              <span>3 itens</span>
+            </CartResumeRow>
+
+            <CartResumeRow total>
+              <p>Valor total</p>
+              <span>R$ 270,00</span>
+            </CartResumeRow>
+
+            <CheckoutButton type="button">Finalizar compra</CheckoutButton>
+          </ModalContainer>
+        </>
+      )}
+    </>
   )
 }
--- src/styles/components/cart-button.ts
+++ src/styles/components/cart-button.ts
@@ -38,3 +38,170 @@ export const CartButtonContainer = styled('button', {
     translate: '7px -7px',
   },
 })
+
+export const ModalOverlay = styled('button', {
+  position: 'absolute',
+  right: 0,
+  top: 0,
+  zIndex: 1,
+  width: '100vw',
+  height: '100vh',
+
+  border: 0,
+  backgroundColor: '$gray900',
+  opacity: 0.3,
+})
+
+export const ModalContainer = styled('aside', {
+  position: 'absolute',
+  right: 0,
+  top: 0,
+  zIndex: 2,
+
+  minHeight: '100vh',
+  width: 480,
+  padding: '24px 48px 48px',
+
+  backgroundColor: '$gray800',
+  boxShadow: '-4px 0px 30px 0px rgba(0, 0, 0, 0.80)',
+
+  display: 'flex',
+  flexDirection: 'column',
+
+  h2: {
+    fontSize: '$lg',
+    color: '$gray100',
+    fontWeight: 'bold',
+    marginTop: 24,
+  },
+})
+
+export const CartResumeRow = styled('div', {
+  marginTop: 7,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  p: {
+    color: '$gray100',
+    fontSize: '1rem',
+    lineHeight: 1.6,
+  },
+  span: {
+    color: '$gray300',
+    fontSize: '$md',
+    lineHeight: 1.6,
+  },
+
+  variants: {
+    total: {
+      true: {
+        fontWeight: 'bold',
+
+        p: {
+          fontSize: '$md',
+          color: '$gray100',
+        },
+
+        span: {
+          fontSize: '$xl',
+          color: '$gray100',
+          lineHeight: 1.4,
+        },
+      },
+    },
+  },
+})
+
+export const CartList = styled('session', {
+  marginTop: 24,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 24,
+
+  flex: 1,
+})
+
+export const CartListItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 20,
+  fontSize: '$md',
+  lineHeight: 1.6,
+
+  img: {
+    background: 'linear-gradient(100deg, #1ea483 0%, #7465d4 100%)',
+    borderRadius: 8,
+    objectFit: 'cover',
+  },
+
+  div: {
+    maxWidth: '100%',
+    overflow: 'hidden',
+  },
+
+  p: {
+    color: '$gray300',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+
+  span: {
+    marginTop: 2,
+    color: '$gray300',
+    fontWeight: 'bold',
+    display: 'block',
+  },
+
+  button: {
+    marginTop: 'auto',
+    fontSize: '1rem',
+    fontWeight: 'bold',
+    color: '$green500',
+    backgroundColor: 'transparent',
+    border: 0,
+    cursor: 'pointer',
+
+    transition: 'all 0.2s',
+
+    '&:hover': {
+      opacity: 0.8,
+    },
+  },
+})
+
+export const CloseModalButton = styled('button', {
+  width: 24,
+  height: 24,
+  background: 'transparent',
+  color: '$gray500',
+  marginLeft: 'auto',
+  alignContent: 'center',
+  border: 0,
+  cursor: 'pointer',
+
+  transition: 'all 0.2s',
+
+  '&:hover': {
+    opacity: 0.8,
+  },
+})
+
+export const CheckoutButton = styled('button', {
+  marginTop: 55,
+  padding: 20,
+  border: 0,
+  borderRadius: 8,
+  fontWeight: 'bold',
+  fontSize: '$md',
+  color: '$white',
+  backgroundColor: '$green500',
+
+  cursor: 'pointer',
+  transition: 'all 0.2s',
+
+  '&:hover': {
+    opacity: 0.8,
+  },
+})
